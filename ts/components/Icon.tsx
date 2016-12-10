d4e637fba594f8bd01bc625b796e93c4a18930bf
Andrew F <andrewfong@esper.com>
2016-12-10 00:10:02 +0000

    DRY up Icon class a little using keyof feature

--- ts/components/Icon.tsx
+++ ts/components/Icon.tsx
@@ -22,92 +22,52 @@ import * as Text from "../text/common";
   Icon names should be semantic. We can map these to appropriate classes
   for font-awesome or other libraries as appropriate.
 */
-type IconType =
-  "accounts"
-  |"cancel"
-  |"close"
-  |"contact"
-  |"dismiss"
-  |"edit"
-  |"filters"
-  |"help"
-  |"home"
-  |"logout"
-  |"privacy"
-  |"save"
-  |"settings"
-  |"terms";
+export const IconMappings = {
+  "accounts": "fa-users",
+  "cancel": "fa-close",
+  "close": "fa-close",
+  "contact": "fa-envelope",
+  "dismiss": "fa-close",
+  "edit": "fa-pencil",
+  "filters": "fa-bars",
+  "help": "fa-question-circle",
+  "home": "fa-home",
+  "logout": "fa-sign-out",
+  "next": "fa-chevron-right",
+  "privacy": "fa-lock",
+  "previous": "fa-chevron-left",
+  "save": "fa-check",
+  "settings": "fa-cog",
+  "terms": "fa-legal"
+};
+
+export type IconType = keyof (typeof IconMappings);
 
 interface Props {
   type: IconType;
   children?: JSX.Element|JSX.Element[]|string;
 }
 
-function getClassForIcon(icon: IconType): string {
-  switch(icon) {
-    case "accounts":
-      return "fa-users";
-    case "cancel":
-      return "fa-close";
-    case "close":
-      return "fa-close";
-    case "contact":
-      return "fa-envelope";
-    case "dismiss":
-      return "fa-close";
-    case "edit":
-      return "fa-pencil";
-    case "filters":
-      return "fa-bars";
-    case "help":
-      return "fa-question-circle";
-    case "home":
-      return "fa-home"
-    case "logout":
-      return "fa-sign-out";
-    case "privacy":
-      return "fa-lock";
-    case "save":
-      return "fa-check";
-    case "settings":
-      return "fa-cog";
-    case "terms":
-      return "fa-legal"
-  }
-}
-
 // Default screen reader label only.
 function getLabelForIcon(icon: IconType): string {
-  switch(icon) {
-    case "accounts":
-      return Text.Accounts;
-    case "cancel":
-      return Text.Cancel;
-    case "close":
-      return Text.Close;
-    case "contact":
-      return Text.Contact;
-    case "dismiss":
-      return Text.Dismiss;
-    case "edit":
-      return Text.Edit;
-    case "filters":
-      return Text.Filters;
-    case "help":
-      return Text.Help;
-    case "home":
-      return Text.Home;
-    case "logout":
-      return Text.Logout;
-    case "privacy":
-      return Text.Privacy;
-    case "save":
-      return Text.Save;
-    case "settings":
-      return Text.Settings;
-    case "terms":
-      return Text.Terms;
-  }
+  return {
+    "accounts": Text.Accounts,
+    "cancel": Text.Cancel,
+    "close": Text.Close,
+    "contact": Text.Contact,
+    "dismiss": Text.Dismiss,
+    "edit": Text.Edit,
+    "filters": Text.Filters,
+    "help": Text.Help,
+    "home": Text.Home,
+    "logout": Text.Logout,
+    "next": Text.Next,
+    "privacy": Text.Privacy,
+    "previous": Text.Previous,
+    "save": Text.Save,
+    "settings": Text.Settings,
+    "terms": Text.Terms
+  }[icon];
 }
 
 /*
@@ -118,7 +78,7 @@ function Icon({ type, children }: Props) {
   let classes = "icon-label";
   if (! hasChildren) { classes += " no-text"; }
   return <span className={classes}>
-    <i className={"fa " + getClassForIcon(type)} />
+    <i className={"fa " + IconMappings[type]} />
     {
       hasChildren ?
       <span>{ children }</span> :
