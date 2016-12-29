a5c2417332c30901d226fd50b9390ec7a6e8d229
Andrew F <andrewfong@esper.com>
2016-12-29 03:16:26 +0000

    Redo group labels selector using new TagList component

--- ts/text/labels.tsx
+++ ts/text/labels.tsx
@@ -2,3 +2,4 @@ export const Label = "Tag";
 export const Labels = "Tags";
 export const Unlabeled = "Untagged";
 export const AddLabel = "Tag Event";
+export const SelectUnlabeled = "Untagged Events Only";
