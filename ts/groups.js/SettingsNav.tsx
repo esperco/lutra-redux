05d18c982e02624ee9dc7ee5df13f9fd5d484372
Keith Yeung <kungfukeith11@gmail.com>
2017-01-24 14:45:24 -0800

    Remove .settings-nav class

--- ts/groups.js/SettingsNav.tsx
+++ ts/groups.js/SettingsNav.tsx
@@ -17,33 +17,35 @@ class Props {
 
 class SettingsNav extends React.Component<Props, {}> {
   render() {
-    return <nav className="settings-nav">
-      <a className={classNames("settings-link", {
-           active: this.props.page === "GeneralSettings"
-         })}
-         href={Paths.generalSettings.href({
-           groupId: this.props.groupId
-         })}>
-        General
-      </a>
-      <a className={classNames("settings-link", {
-           active: this.props.page === "LabelSettings"
-         })} href={Paths.setup.href({})}>
-        Tags
-      </a>
-      <a className={classNames("settings-link", {
-           active: this.props.page === "NotificationSettings"
-         })}
-         href={Paths.setup.href({})}>
-        Notifications
-      </a>
-      <a className={classNames("settings-link", {
-           active: this.props.page === "BillingSettings"
-         })}
-         href={Paths.setup.href({})}>
-        Billing
-      </a>
-    </nav>;
+    return <header>
+      <nav>
+        <a className={classNames("settings-link", {
+             active: this.props.page === "GeneralSettings"
+           })}
+           href={Paths.generalSettings.href({
+             groupId: this.props.groupId
+           })}>
+          General
+        </a>
+        <a className={classNames("settings-link", {
+             active: this.props.page === "LabelSettings"
+           })} href={Paths.setup.href({})}>
+          Tags
+        </a>
+        <a className={classNames("settings-link", {
+             active: this.props.page === "NotificationSettings"
+           })}
+           href={Paths.setup.href({})}>
+          Notifications
+        </a>
+        <a className={classNames("settings-link", {
+             active: this.props.page === "BillingSettings"
+           })}
+           href={Paths.setup.href({})}>
+          Billing
+        </a>
+      </nav>
+    </header>;
   }
 }
 
