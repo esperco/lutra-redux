4ccad2001d784957b98e7a0b4c4f2fb25772260f
Andrew F <andrewfong@esper.com>
2017-02-16 22:21:31 +0000

    Add toggle in group settings for Esper Link

---
+++ ts/text/misc.tsx
@@ -0,0 +1,5 @@
+export const EsperLink = "Insert link inside calendar event";
+export const EsperLinkDescription =
+  "Insert a link inside each event's description that opens up that event " +
+  "on Esper. All event guests will be able to see this link, but any data " +
+  "you save on Esper itself will be private to yourself or your organization.";
